0c8c637fa9a71f3e1c7868ed0cc5bcbae590eb95
alimahatma <laluallistilo@gmail.com>
2021-09-17 15:54:08 +0100

    perintah WHERE untuk menampilkan data dengan values tertentu

--- SELECT.sql
+++ SELECT.sql
@@ -17,3 +17,6 @@ SELECT nama_kolom FROM nama_tabel;
  /*Menampilkan Kolom Country dengan memfilter nilai yang sama (duplicate)*/
  SELECT DISTINCT country_of_birth FROM person ORDER BY country_of_birth;
  SELECT DISTINCT country_of_birth FROM person ORDER BY country_of_birth DESC;
+
+ /*menampilkan data berdasarkan nilai atribut gender*/
+  SELECT * FROM person WHERE gender ='Male'; /* akan menampilkan semua data yang bernilai 'Male' */
