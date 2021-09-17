bcfdb34611f04556f8049e29f3a8cb8629011dca
Mutawalli. L <32710249+alimahatma@users.noreply.github.com>
2021-09-17 15:47:50 +0100

    perintah SELECT, ORDER and DISTINCT

---
+++ SELECT.sql
@@ -0,0 +1,19 @@
+/*Memanggil seluruh isi data pada tabel*/
+SELECT * FROM nama_tabel;
+
+/*Memanggil beberapa kolom*/
+SELECT nama_kolom FROM nama_tabel;
+
+
+/*ORDER BY ASSCENDING (ASC) DESSCENDING (DESC)*/
+ SELECT * FROM person ORDER by id DESC;
+ SELECT * FROM person ORDER by id ASC;
+ SELECT * FROM person ORDER by id, email DESC;
+
+ /*Menampilkan Kolom Country dengan urutan Abjad ABCDE.... ZYX...*/
+ SELECT country_of_birth FROM person ORDER BY country_of_birth;
+ SELECT country_of_birth FROM person ORDER BY country_of_birth DESC;
+
+ /*Menampilkan Kolom Country dengan memfilter nilai yang sama (duplicate)*/
+ SELECT DISTINCT country_of_birth FROM person ORDER BY country_of_birth;
+ SELECT DISTINCT country_of_birth FROM person ORDER BY country_of_birth DESC;
