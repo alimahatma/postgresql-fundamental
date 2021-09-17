57492ffef8a4902833d45a96294ebe8869589459
Mutawalli. L <32710249+alimahatma@users.noreply.github.com>
2021-09-17 15:46:05 +0100

    perintah membuat dan mengelola database

---
+++ database.sql
@@ -0,0 +1,14 @@
+/*Koneksi database (memilih database)*/
+/* ---> \c nama_database <--- */
+
+/*Membuat Databse*/
+CREATE DATABASE nama_database;
+
+/*Menghapus Database*/
+DROP DATABASE 
+
+/* ---> memasukkan data kedalam tabel GENERATE 1000 <--- */
+/* ---> https://www.mockaroo.com/ <--- */
+/* ---> \i lokasi_database "C:/Users/sasambo-techno/Downloads/person.sql" <--- */
+
+
